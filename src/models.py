c8648ae14c65dc4e2ed98dff66012f663cb6f085
Céline Liu <celine.liu@telecom-paris.fr>
2026-03-07 14:12:41 +0100

    removed batchnorm & added modular activation

--- src/models.py
+++ src/models.py
@@ -2,18 +2,33 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-# https://github.com/milesial/Pytorch-UNet
+
+def get_activation(name):
+    name = name.lower()
+
+    if name == "relu":
+        return nn.ReLU(inplace=True)
+
+    if name == "leakyrelu":
+        return nn.LeakyReLU(0.1, inplace=True)
+
+    if name == "silu":
+        return nn.SiLU(inplace=True)
+
+    raise ValueError("Activation must be one of ['relu','leakyrelu','silu']")
+
 class DoubleConv(nn.Module):
-    """(convolution => [BN] => ReLU) * 2"""
-    def __init__(self, in_channels, out_channels):
+    """(convolution => activation) * 2"""
+    def __init__(self, in_channels, out_channels, activation="silu"):
         super().__init__()
+
         self.double_conv = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
+            #nn.BatchNorm2d(out_channels),
+            get_activation(activation),
             nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True)
+            #nn.BatchNorm2d(out_channels),
+            get_activation(activation),
         )
 
     def forward(self, x):
@@ -32,11 +47,13 @@ class BilinearUpConv(nn.Module):
         return self.conv(x)
     
 class GradualExpansionUNet(nn.Module):
-    def __init__(self, in_msi=12, in_hsi=230, interpolation_mode='ConvTranspose2d'):
+    def __init__(self, in_msi=12, in_hsi=230, interpolation_mode='ConvTranspose2d', activation='silu'):
         """
            interpolation_mode in ['ConvTranspose2d', 'Bilinear']. With ConvTranspose2d, 
            the upsampling is learnable (and could therefore introduce artefacts). With Bilinear, 
            the upsampling is smoother.
+
+           activation in ['relu', 'leakyrelu', 'silu']
         """
         super().__init__()
 
@@ -46,11 +63,11 @@ class GradualExpansionUNet(nn.Module):
         self.interpolation_mode = interpolation_mode
 
         # [B, 12, H, W]
-        self.inc = DoubleConv(in_msi, 64) # [B, 64, H, W]
+        self.inc = DoubleConv(in_msi, 64, activation) # [B, 64, H, W]
 
-        self.down1 = nn.Sequential(nn.MaxPool2d(2), DoubleConv(64, 128))  # [B, 128, H/2, W/2]
-        self.down2 = nn.Sequential(nn.MaxPool2d(2), DoubleConv(128, 256)) # [B, 256, H/4, W/4]
-        self.down3 = nn.Sequential(nn.MaxPool2d(2), DoubleConv(256, 512)) # [B, 512, H/8, W/8]
+        self.down1 = nn.Sequential(nn.MaxPool2d(2), DoubleConv(64, 128, activation))  # [B, 128, H/2, W/2]
+        self.down2 = nn.Sequential(nn.MaxPool2d(2), DoubleConv(128, 256, activation)) # [B, 256, H/4, W/4]
+        self.down3 = nn.Sequential(nn.MaxPool2d(2), DoubleConv(256, 512, activation)) # [B, 512, H/8, W/8]
 
         if self.interpolation_mode == 'ConvTranspose2d':
             self.up1a = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2) # [B, 256, H/4, W/4]
@@ -63,11 +80,11 @@ class GradualExpansionUNet(nn.Module):
 
 
         # Concat with down2: [B, 256+256, H/4, W/4]
-        self.up1b = DoubleConv(512, 384)    # [B, 384, H/4, W/4]
+        self.up1b = DoubleConv(512, 384, activation)    # [B, 384, H/4, W/4]
         # Concat with down1: [B, 256+128, H/2, W/2]
-        self.up2b = DoubleConv(384, 256)    # [B, 256, H/2, W/2]
+        self.up2b = DoubleConv(384, 256, activation)    # [B, 256, H/2, W/2]
         # Concat with inc: [B, 192+64, H, W]
-        self.up3b = DoubleConv(256, 230)    # [B, 230, H, W]
+        self.up3b = DoubleConv(256, 230, activation)    # [B, 230, H, W]
         
         self.outc = nn.Conv2d(230, in_hsi, kernel_size=1) # [B, 230, H, W]
 
@@ -92,29 +109,31 @@ class GradualExpansionUNet(nn.Module):
         return self.outc(x)      # [B, 230, H, W]
 
 class UNet(nn.Module):
-    def __init__(self, in_channels=230, base_features=64, interpolation_mode='ConvTranspose2d', learning_mode='Standard'):
+    def __init__(self, in_channels=230, base_features=64, interpolation_mode='ConvTranspose2d', learning_mode='standard', activation='silu'):
         """
            interpolation_mode in ['ConvTranspose2d', 'Bilinear']. With ConvTranspose2d, 
            the upsampling is learnable (and could therefore introduce artefacts). With Bilinear, 
            the upsampling is smoother.
 
-           learning_mode in ['Standard', 'Residual']
+           learning_mode in ['standard', 'residual']
+
+           activation in ['relu', 'leakyrelu', 'silu']
         """
         super().__init__()
 
         if interpolation_mode not in ['ConvTranspose2d', 'Bilinear']:
             raise ValueError("interpolation_mode must be 'ConvTranspose2d' or 'Bilinear'")
         
-        if learning_mode not in ['Standard', 'Residual']:
-            raise ValueError("learning_mode must be 'Standard' or 'Residual'")
+        if learning_mode not in ['standard', 'residual']:
+            raise ValueError("learning_mode must be 'standard' or 'residual'")
 
         self.interpolation_mode = interpolation_mode
         self.learning_mode = learning_mode
 
-        self.inc = DoubleConv(in_channels, base_features)           # [B, 64, H, W]
-        self.down1 = nn.Sequential(nn.MaxPool2d(2), DoubleConv(base_features, base_features*2))   # [B, 128, H/2, W/2]
-        self.down2 = nn.Sequential(nn.MaxPool2d(2), DoubleConv(base_features*2, base_features*4)) # [B, 256, H/4, W/4]
-        self.down3 = nn.Sequential(nn.MaxPool2d(2), DoubleConv(base_features*4, base_features*8)) # [B, 512, H/8, W/8]
+        self.inc = DoubleConv(in_channels, base_features, activation)           # [B, 64, H, W]
+        self.down1 = nn.Sequential(nn.MaxPool2d(2), DoubleConv(base_features, base_features*2, activation))   # [B, 128, H/2, W/2]
+        self.down2 = nn.Sequential(nn.MaxPool2d(2), DoubleConv(base_features*2, base_features*4, activation)) # [B, 256, H/4, W/4]
+        self.down3 = nn.Sequential(nn.MaxPool2d(2), DoubleConv(base_features*4, base_features*8, activation)) # [B, 512, H/8, W/8]
 
         if self.interpolation_mode == 'ConvTranspose2d':
             self.up1a = nn.ConvTranspose2d(base_features*8, base_features*4, kernel_size=2, stride=2)  # [B, 256, H/4, W/4]
@@ -125,9 +144,9 @@ class UNet(nn.Module):
             self.up2a = BilinearUpConv(base_features*4, base_features*2)
             self.up3a = BilinearUpConv(base_features*2, base_features)
 
-        self.up1b = DoubleConv(base_features*8, base_features*4)    
-        self.up2b = DoubleConv(base_features*4, base_features*2)    
-        self.up3b = DoubleConv(base_features*2, base_features)    
+        self.up1b = DoubleConv(base_features*8, base_features*4, activation)    
+        self.up2b = DoubleConv(base_features*4, base_features*2, activation)    
+        self.up3b = DoubleConv(base_features*2, base_features, activation)    
 
         self.outc = nn.Conv2d(base_features, in_channels, kernel_size=1) 
 
@@ -153,6 +172,6 @@ class UNet(nn.Module):
         
         res = self.outc(out)  
 
-        if self.learning_mode == 'Standard':
+        if self.learning_mode == 'standard':
             return res
         return id + res
